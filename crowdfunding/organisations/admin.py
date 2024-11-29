d3494c9e916765349867bef15b6e3a3f532ce7c7
GinaHorch <reggiaus@hotmail.com>
2024-11-29 15:06:11 +0800

    fix duplicate registration

--- crowdfunding/organisations/admin.py
+++ crowdfunding/organisations/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import OrganisationProfile
 
 # Register your models here.
-admin.site.register(OrganisationProfile)
-
 @admin.register(OrganisationProfile)
 class OrganisationProfileAdmin(admin.ModelAdmin):
     list_display = ['id', 'organisation_name', 'organisation_contact', 'organisation_ABN', 'is_charity']
