17890e9cc96a766f6379366cceed4d4227f024d9
GinaHorch <reggiaus@hotmail.com>
2024-12-06 08:52:48 +0800

    Add url to users

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -5,12 +5,14 @@ from .views import (
     CustomUserList, 
     CustomUserDetail,
     OrganisationList,
-    OrganisationDetail
+    OrganisationDetail,
+    SignupView
 )
 
 urlpatterns = [
     path('', CustomUserList.as_view(), name='user-list'), # List and create users
     path('<int:pk>/', CustomUserDetail.as_view(), name='user-detail'), # Get, update, delete a user
+    path('signup/', SignupView.as_view(), name='signup'),
     path('api/token-auth/', TokenAuthView.as_view(), name='token-auth'), # Unified token authentication
     path('organisations/', OrganisationList.as_view(), name='organisation-list'),
     path('organisations/<int:pk>/', OrganisationDetail.as_view(), name='organisation-detail'),
