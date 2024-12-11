887fb0e37745293bfb85ee616fec25bba516ed85
GinaHorch <reggiaus@hotmail.com>
2024-12-12 07:01:11 +0800

    Login 404 error persists - changes to url paths

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', CustomUserList.as_view(), name='user-list'), # List and create users
     path('<int:pk>/', CustomUserDetail.as_view(), name='user-detail'), # Get, update, delete a user
     path('signup/', SignupView.as_view(), name='signup'),
-    path('api/token-auth/', TokenAuthView.as_view(), name='token-auth'), # Unified token authentication
+    path('token-auth/', TokenAuthView.as_view(), name='token-auth'), # Unified token authentication
     path('organisations/', OrganisationList.as_view(), name='organisation-list'),
     path('organisations/<int:pk>/', OrganisationDetail.as_view(), name='organisation-detail'),
 ]
