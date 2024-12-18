b09f28f8eda789302e7a05396205180bbf615de3
GinaHorch <reggiaus@hotmail.com>
2024-12-18 09:03:11 +0800

    Simplify app-level paths

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -8,11 +8,11 @@ def debug_view(request):
 urlpatterns = [
   path('', ProjectList.as_view(), name='project-list'),
   path('create/', ProjectCreate.as_view(), name='project-create'),
-  path('projects/<int:pk>/', ProjectDetail.as_view(), name='project-detail'),
+  path('<int:pk>/', ProjectDetail.as_view(), name='project-detail'),
 
   path('pledges/', PledgeList.as_view(), name='pledge-list'),
   path('pledges/<int:pk>/', PledgeDetail.as_view(), name='pledge-detail'),
-  path('projects/<int:project_id>/pledges/', ProjectPledgeCreateView.as_view(), name='project-pledges'),
+  path('<int:project_id>/pledges/', ProjectPledgeCreateView.as_view(), name='project-pledges'),
 
   path('categories/', CategoryListCreate.as_view(), name='categories-list-create'),
   
