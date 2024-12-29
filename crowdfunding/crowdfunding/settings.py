0e466ac29fa00065e36cd49a967e835ed0ee1fd3
GinaHorch <reggiaus@hotmail.com>
2024-12-29 17:22:53 +0800

    Configure AWS and remove whitenoise as staticfile manager

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -107,7 +107,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
