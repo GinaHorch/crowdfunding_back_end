b0f9565a0ca4366ad8179bd926e620cf96240055
GinaHorch <reggiaus@hotmail.com>
2024-12-29 20:18:21 +0800

    Issue with uploading images - adjusted settings.py

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -191,7 +191,7 @@ AWS_S3_REGION_NAME = 'ap-southeast-2'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
 
 # Static Files
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/static/"
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 # only required locally
@@ -201,7 +201,7 @@ STATICFILES_DIRS = [
 ]
 
 # Media Files
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Additional S3 Settings for Public/Private Access Control
