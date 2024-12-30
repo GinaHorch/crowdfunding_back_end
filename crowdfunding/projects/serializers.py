369ed9b9e9f02c4f2335db23218b451b43187fa4
GinaHorch <reggiaus@hotmail.com>
2024-12-30 18:03:49 +0800

    Revert change to Project Serializer to resolve newly created viewing issue.

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -18,7 +18,7 @@ class PledgeSerializer(serializers.ModelSerializer):
 
 class ProjectSerializer(serializers.ModelSerializer):
    organisation = serializers.ReadOnlyField(source='organisation.id')
-   image = serializers.URLField(allow_null=True, required=False)
+   image = serializers.ImageFieldField(allow_null=True, required=False)
    class Meta:
       model = Project
       fields = ['id', 'title', 'description', 'target_amount', 
