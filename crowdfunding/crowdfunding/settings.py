c6804602c6abf03ab20d93d3a0e3a8d8e794cee6
GinaHorch <reggiaus@hotmail.com>
2024-12-30 15:10:01 +0800

    Add media root towards resolving issue of images not uploading to S3

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -233,6 +233,7 @@ STATICFILES_DIRS = [
 # Media Files
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_ROOT = 'media/'
 
 # Additional S3 Settings for Public/Private Access Control
 AWS_QUERYSTRING_AUTH = False  # Disable query string authentication for public URLs
