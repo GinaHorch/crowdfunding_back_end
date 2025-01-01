7d1dac0aef0a9eac6241636a103aeb9db5322cbf
GinaHorch <reggiaus@hotmail.com>
2025-01-01 18:02:42 +0800

    re-remove media from media url but leave /

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -231,7 +231,7 @@ STATICFILES_DIRS = [
 ]
 
 # Media Files
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_ROOT = ""
 
